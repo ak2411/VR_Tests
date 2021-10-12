09908729e8f73f99f78abaf709aa17a515086002
ak2411 <ak2725@cornell.edu>
2021-10-12 19:06:10 -0400

    Current progress.

---
+++ Assets/DebugFunctions.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugFunctions : MonoBehaviour
+{
+    public void print(string message){
+        Debug.Log(message);
+    }
+}
